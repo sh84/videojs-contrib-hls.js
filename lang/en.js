2c1b8986ef44654e25093ec878f6ca6c08d5ab02
Pravdina Elena <helenj@inventos.ru>
2017-05-31 11:46:54 +0300

    add 'no level...' error localization

--- lang/en.js
+++ lang/en.js
@@ -1,5 +1,6 @@
 videojs.addLanguage('en', {
 	"Error loading media: File could not be played": "Error loading media: File could not be played",
-	"Too many errors. Last error: " : "Too many errors. Last error: ",
-	"networkError" : "networkError"
+	"Too many errors. Last error: ": "Too many errors. Last error: ",
+	"networkError": "networkError",
+	"no level found in manifest": "no level found in manifest. Contact technical support"
 });
